f3cf7c7fc5697ded1618776c692a45a0c45aca0e
Xyplixt <aryansinghfzd2003@gmail.com>
2024-06-08 06:48:57 +0530

    Initial commit with my configuration files

---
+++ arch/.config/nvim/lua/plugins/code_runner.lua
@@ -0,0 +1,27 @@
+return {
+	"CRAG666/code_runner.nvim",
+	config = function()
+		require("code_runner").setup({
+			filetype = {
+				java = {
+					"cd $dir &&",
+					"javac $fileName &&",
+					"java $fileNameWithoutExt",
+				},
+				python = "python3 -u",
+				typescript = "deno run",
+				rust = {
+					"cd $dir &&",
+					"rustc $fileName &&",
+					"$dir/$fileNameWithoutExt",
+				},
+				c = {
+					"cd $dir &&",
+					"gcc $fileName -o /tmp/$fileNameWithoutExt &&",
+					"/tmp/$fileNameWithoutExt &&",
+					"rm /tmp/$fileNameWithoutExt",
+				},
+			},
+		})
+	end,
+}
